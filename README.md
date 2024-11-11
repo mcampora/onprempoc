26194980e42dde18c57cb8a4157d90e2935acaf3
mcampora-ebsco <mcampora@ebsco.com>
2024-11-11 11:09:01 -0500

    tested 2 profiles

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ To make it work, you have to provision:
 - in AWS IAM Role Anywhere create a trust anchor and profile with this root CA and an AWS IAM Role granting S3 read permissions
 - in AWS Certificate Manager create a certificate signed with the root CA
 - download the certificate, certificate chain, private key and install them in a ./pca subfolder
-- add a .env finle in this ./pca folder containing:
+- add a .env file in this ./pca folder containing:
+    - TRUST_ANCHOR_ARN="arn:aws:rolesanywhere:region:account:trust-anchor/anchor"
     - PROFILE_ARN="arn:aws:rolesanywhere:region:account:profile/profile"
     - ROLE_ARN="arn:aws:iam::account:role/role"
-    - TRUST_ANCHOR_ARN="arn:aws:rolesanywhere:region:account:trust-anchor/anchor"
     - CERT_PATH="./pca/certificate.txt"
     - PRIVATE_KEY_PATH="./pca/private_key.txt"
     - PRIVATE_KEY_PASSPHRASE="my_secret"
--- test.py
+++ test.py
@@ -13,10 +13,12 @@ import boto3
 #call .env file with non-sensitive AWS details
 env_val = dotenv_values("pca/.env")
 
+# first session with a profile with S3 access
+#------------------------------------------------
 roles_anywhere_session = IAMRolesAnywhereSession(
+    trust_anchor_arn=env_val.get('TRUST_ANCHOR_ARN'),
     profile_arn=env_val.get('PROFILE_ARN'),
     role_arn=env_val.get('ROLE_ARN'),
-    trust_anchor_arn=env_val.get('TRUST_ANCHOR_ARN'),
     certificate=env_val.get('CERT_PATH'),
     private_key=env_val.get('PRIVATE_KEY_PATH'),
     private_key_passphrase=env_val.get('PRIVATE_KEY_PASSPHRASE'),
@@ -30,3 +32,23 @@ response = client.list_buckets()
 print('Existing buckets:')
 for bucket in response['Buckets']:
     print(f'  {bucket["Name"]}')
+
+# second session with a profile with SSM access
+#------------------------------------------------
+roles_anywhere_session = IAMRolesAnywhereSession(
+    trust_anchor_arn=env_val.get('TRUST_ANCHOR_ARN'),
+    profile_arn=env_val.get('PROFILE2_ARN'),
+    role_arn=env_val.get('ROLE2_ARN'),
+    certificate=env_val.get('CERT_PATH'),
+    private_key=env_val.get('PRIVATE_KEY_PATH'),
+    private_key_passphrase=env_val.get('PRIVATE_KEY_PASSPHRASE'),
+    region='us-east-1',
+).get_session()
+
+client = roles_anywhere_session.client('secretsmanager')
+
+# test the ssm access by listing the secrets
+response = client.list_secrets()
+print('Existing secrets:')
+for secret in response['SecretList']:
+    print(f'  {secret["Name"]}')
